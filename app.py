016110ee9a29269739f5846dd5d57f8d8ebbde29
DaniyalAhmadKhan-LUMS <22100215@lums.edu.pk>
2023-07-18 00:13:18 +0500

    final push

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, request, jsonify
 from langchain.document_loaders import PyPDFLoader
 import preprocessPDF
 import generateQuiz
+import PyPDF2
 from flask_cors import CORS, cross_origin 
+import base64
+import io
 
 app = Flask(__name__)
 CORS(app)
@@ -25,13 +28,19 @@ def index():
 @cross_origin()
 def process_pdf():
     data = request.get_json()  # Get JSON body of request
-    pdf_name = data.get('pdfName')  # Extract pdfName from JSON
+    encodings = data.get('encoding')  # Extract pdfName from JSON
 
     # Ensure that a PDF name was provided
-    if not pdf_name:
-        return jsonify({'error': 'No PDF name provided'}), 400
-
-    text = readPDF(pdf_name)
+    if not encodings:
+        return jsonify({'error': 'Incorrect data'}), 400
+    base64_str = encodings.split(',')[1]
+    file_data = base64.b64decode(base64_str)
+    file_obj = io.BytesIO(file_data)
+    pdf_reader = PyPDF2.PdfReader(file_obj)
+    text = ''
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
     chunks = preprocessPDF.text_to_doc(text)
     
 
@@ -44,13 +53,83 @@ def chunkQuiz():
 
     # Ensure that a chunk was provided
     if not chunk:
-        return jsonify({'error': 'No PDF name provided'}), 400
+        return jsonify({'error': 'Incorrect data'}), 400
 
-    questions = generateQuiz.generate(chunk)
+    questionsMCQs = generateQuiz.mcqGenerate(chunk)
+    # questionsFAQs = generateQuiz.faqGenerate(chunk)
+    # summary = generateQuiz.generateSummary(chunk)
+    questions = questionsMCQs
+    # questions = questionsMCQs+questionsFAQs
     
     
 
-    return jsonify(questions), 200  # Returns JSON response
+    return jsonify({"questions":questions,"summary":"summary"}), 200  # Returns JSON response
+
+@app.route('/quizMCQ', methods=['POST'])
+@cross_origin()
+def quizMCQs():
+    data = request.get_json()  # Get JSON body of request
+    chunk = data.get('chunk')  # Extract chunk from JSON
+
+    if not chunk:
+        return jsonify({'error': 'Incorrect data'}), 400
+
+    questionsMCQs = generateQuiz.mcqGenerate(chunk)
+    questions = questionsMCQs
+    
+    
+
+    return jsonify({"questionsMCQs":questions}), 200  # Returns JSON response
+
+@app.route('/quizFAQ', methods=['POST'])
+@cross_origin()
+def quizFAQs():
+    data = request.get_json()  # Get JSON body of request
+    chunk = data.get('chunk')  # Extract chunk from JSON
+
+    # Ensure that a chunk was provided
+    if not chunk:
+        return jsonify({'error': 'Incorrect data'}), 400
+
+    questionsFAQs = generateQuiz.faqGenerate(chunk)
+    questions = questionsFAQs
+    
+    
+
+    return jsonify({"questionsFAQs":questions}), 200  # Returns JSON response
+
+@app.route('/summary', methods=['POST'])
+@cross_origin()
+def quizSummary():
+    data = request.get_json()  # Get JSON body of request
+    chunk = data.get('chunk')  # Extract chunk from JSON
+
+    # Ensure that a chunk was provided
+    if not chunk:
+        return jsonify({'error': 'Incorrect data'}), 400
+    summary = generateQuiz.generateSummary(chunk)
+    
+    
+
+    return jsonify({"summary":summary}), 200  # Returns JSON response
+
+@app.route('/gradeFAQ', methods=['POST'])
+@cross_origin()
+def gradeFAQ():
+    data = request.get_json()  # Get JSON body of request
+    chunk = data.get('chunk')  # Extract chunk from JSON
+    answers = data.get('answers')
+    question = data.get('question')
+    # Ensure that a chunk was provided
+    if not chunk or not answers or not question:
+        return jsonify({'error': 'Incorrect data'}), 400
+    graded = generateQuiz.faqGrade({"chunk":chunk,"answers":answers,"question":question})
+    
+    
+
+    return graded, 200  # Returns JSON response
 
 if __name__ == "__main__":
-    app.run()  # run the app on port 5000
+    app.run(debug=True, port=5000)  # run the app on port 5000
+    # app.run()  # run the app on port 5000
+
--- generateQuiz.py
+++ generateQuiz.py
@@ -2,16 +2,20 @@
 from langchain.prompts import PromptTemplate
 from langchain import LLMChain, PromptTemplate
 from langchain.chat_models import ChatOpenAI
-
-
-
-
+from langchain.chains.summarize import load_summarize_chain
+import openai
+import re
+import json
+openai.api_key = "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"
 OPENAI_API_KEY = "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"
-model_name='gpt-4'
+# model_name='gpt-4'
 
-# model_name='gpt-3.5-turbo'
+model_name='gpt-3.5-turbo'
 llm = ChatOpenAI(model_name=model_name,temperature=0,openai_api_key=OPENAI_API_KEY)
-
+def clean_string(s):
+    # Replace "A. ", "B. ", "C. ", "D. " and following whitespace with an empty string
+    cleaned_s = re.sub(r'[ABCD]\.\s', '', s)
+    return cleaned_s
 def generateMCQS(text):
   prompt_templateMCQS = """You are a university professor making a quiz with your knowledge for your student which test conceptual and critical thinking skills
     of a student. I'll provide you with some information and
@@ -24,28 +28,20 @@ def generateMCQS(text):
 
   Question: {question}
 
-  please generate the response in following format:
+    Please provide the evaluation in the form of a json. Following is the format of the json:
+    [{{   
+    "Question": "question",
+    "Answer A": {{"Option A":"option A","Explanation A": "why the answer A is correct or incorrect"}},
+    "Answer B": {{"Option B":"option B","Explanation B": "why the answer B is correct or incorrect"}},
+    "Answer C": {{"Option C":"option C","Explanation C": "why the answer C is correct or incorrect"}},
+    "Answer D": {{"Option D":"option D","Explanation D": "why the answer D is correct or incorrect"}},
+    "Correct Option": "Correct option as, 'A', 'B', 'C', or 'D'"
+    
+    }}]
 
-  Question Statement
-    a) Option A
-    b) Option B
-    c) Option C
-    d) Option D
-
-  Option A: Explanation of option A
-
-  Option B: Explanation of option B
-
-  Option C: Explanation of option C
-
-  Option D: Explanation of option D
-
-  Correct Option: [Correct option as, "A", "B", "C", or "D"]
-
-  ####
   Note: Don't give the options that are partially correct.
 
-  I want to ensure that the format remains the same for each of the questions, options, correct answers, explanations, and ####. Thank you.
+  I want to ensure that the format remains the same for each of the questions, options, correct answers, and explanations. Thank you.
   The questions should be open-ended, add examples if possible, focus on why more and at the high difficulty level for a university student.  
 
   """
@@ -54,8 +50,9 @@ def generateMCQS(text):
   )
 
   chainMCQS = LLMChain(llm=llm , prompt=PROMPTMCQS)
-  questionMCQS = "Please generate in-depth multiple-choice questions, each question should have four options. The questions should be about the main concepts, insights, and implications. Also mention in the explaination why the oprion is correct and why its not correct"
+  questionMCQS = "Please generate two in-depth multiple-choice questions, each question should have four options. The questions should be about the main concepts, insights, and implications. Also mention in the explaination why the oprion is correct and why its not correct"
   ouptutMCQS = chainMCQS.predict(context=text, question=questionMCQS)
+  # print(ouptutMCQS)
   return ouptutMCQS
 
 
@@ -79,7 +76,15 @@ def generateFAQs(text):
 
   ####
 
-  I want to ensure that the format remains the same for each of the questions, answers, explanations, and ####. Thank you.
+    Please provide the evaluation in the form of a json. Following is the format of the json:
+    [{{   
+    "Question": "question",
+    "Answer A": {{"Option A":"first answer","Explanation A": "why the first answer is correct or incorrect"}},
+    "Answer B": {{"Option B":"second answer","Explanation B": "why the second answer is correct or incorrect"}},
+    "Answer C": {{"Option C":"third answer","Explanation C": "why the third answer is correct or incorrect"}}
+    }}]
+
+  I want to ensure that the format remains the same for each of the questions, answers, and explanations. Thank you.
   The questions should be open-ended, add examples if possible, focus on why more and at the high difficulty level for a university student.  
 
   """
@@ -87,32 +92,105 @@ def generateFAQs(text):
       template=prompt_templateFAQS, input_variables=["context", "question"]
   )
   chainFAQS = LLMChain(llm=llm , prompt=PROMPTFAQS)
-  questionFAQS = "Please generate in-depth FAQ questions. The questions should be about the main concepts, insights, and implications."
+  questionFAQS = "Please generate two in-depth FAQ questions. The questions should be about the main concepts, insights, and implications."
   ouptutFAQS = chainFAQS.predict(context=text, question=questionFAQS)
   return ouptutFAQS
 
 
 
-def generate(chunck):
+def mcqGenerate(chunck):
     MCQ = generateMCQS(chunck)
+    # print(MCQ)
+    listQuestions = ''
+    try:
+      listQuestions = json.loads(MCQ)
+    except:
+       pass
+    # print(listQuestions)
     listFinal = []
-    listQuestions = MCQ.split("####")
     for x in listQuestions:
-        questionDic = {}
-        extracted = x.strip()
-        extracted = extracted.split("\n")
         cleanExtracted = []
-        for y in extracted:
-            if y != '':
-                cleanExtracted.append(y.strip())
+        questionDic = {}
         try:
-            questionDic["question"] = cleanExtracted[0].split(":")[-1].strip()
+            questionDic["question"] = x["Question"]
             questionDic["type"] = "MCQS"
-            questionDic["Choices"] = [cleanExtracted[1].split(")")[-1].strip(),cleanExtracted[2].split(")")[-1].strip(),cleanExtracted[3].split(")")[-1].strip(),cleanExtracted[4].split(")")[-1].strip()]
-            questionDic["choicesDescriptions"] = [cleanExtracted[5].replace("[", "").replace("]", "").split(":")[-1].strip(),cleanExtracted[6].replace("[", "").replace("]", "").split(":")[-1].strip(),cleanExtracted[7].replace("[", "").replace("]", "").split(":")[-1].strip(),cleanExtracted[8].replace("[", "").replace("]", "").split(":")[-1].strip()]
-            questionDic["correctAnswer"] = cleanExtracted[9][-1]
+            questionDic["choices"] = [clean_string(x["Answer A"]["Option A"]),clean_string(x["Answer B"]["Option B"]),clean_string(x["Answer C"]["Option C"]),clean_string(x["Answer D"]["Option D"])]
+            questionDic["choicesDescriptions"] = [x["Answer A"]["Explanation A"],x["Answer B"]["Explanation B"],x["Answer C"]["Explanation C"],x["Answer D"]["Explanation D"]]
+            questionDic["correctAnswer"] = x["Correct Option"]
             listFinal.append(questionDic)
         except:
             pass
     return listFinal
+def faqGenerate(chunck):
+    FAQ = generateFAQs(chunck)
+    # print(FAQ)
+    listQuestions = ''
+    try:
+      listQuestions = json.loads(FAQ)
+    except:
+       pass
+    listFinal = []
+    # print(listQuestions)
+    for x in listQuestions:
+        questionDic = {}
+        try:
+            questionDic["question"] = x["Question"]
+            questionDic["type"] = "Descriptive"
+            questionDic["topics"] = [clean_string(x["Answer A"]["Option A"]),clean_string(x["Answer B"]["Option B"]),clean_string(x["Answer C"]["Option C"])]
+            questionDic["choicesDescriptions"] = [x["Answer A"]["Explanation A"],x["Answer B"]["Explanation B"],x["Answer C"]["Explanation C"]]
+            listFinal.append(questionDic)
+        except:
+            pass
+    # print(listFinal)
+    return listFinal
+def generateSummary(chunk):
+  openai_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You need to summarize this text and don't mention its a summary."},
+            {"role": "user", "content": chunk}
+        ]
+    )
+  # print(openai_response['choices'][0]['message']['content'])
+  return openai_response['choices'][0]['message']['content']
+
+
+
+def gradeFAQs(dictQuestion):
+  prompt_grading = """
+    Context: {context}
+
+    Question: {question}
+
+    Student's Answers: 
+    A. {student_answer1}
+    B. {student_answer2}
+    C. {student_answer3}
+
+    You are a teacher grading a quiz based on given answers, and context. Evaluate the responses and grade them. The \
+      grade should be out of 10. You need to give equal weightage to all of the answers \
+      For every correct options give give 3 marks. For partially correct option give 1 \
+      mark. If all the options are correct give 10 marks. Grade will be of the overall question.  
+      Please provide the evaluation in the form of a json. Following is the format of the json:
+    {{   
+    "Question": "question",
+    "Answer A": {{"Option A":"first answer","Explanation A": "why the first answer is correct or incorrect"}},
+    "Answer B": {{"Option B":"second answer","Explanation B": "why the second answer is correct or incorrect"}},
+    "Answer C": {{"Option C":"third answer","Explanation C": "why the third answer is correct or incorrect"}},
+    "Grade": "total grade of the question"
+    }}
+
+    """
+  PROMPTFAQS = PromptTemplate(
+      template=prompt_grading, input_variables=["context", "question","student_answer1","student_answer2","student_answer3"]
+  )
+  chainFAQS = LLMChain(llm=llm , prompt=PROMPTFAQS)
+  ouptutFAQS = chainFAQS.predict(context=dictQuestion["chunk"], question=dictQuestion["question"],student_answer1=dictQuestion["answers"][0],student_answer2=dictQuestion["answers"][1],student_answer3=dictQuestion["answers"][2])
+  return ouptutFAQS
+
+def faqGrade(dictQuestion):
+    FAQ = gradeFAQs(dictQuestion)
+    dict_obj = json.loads(FAQ)
+    response_final = {"explanations":[dict_obj["Answer A"]["Explanation A"],dict_obj["Answer B"]["Explanation B"],dict_obj["Answer C"]["Explanation C"]], "grade": dict_obj["Grade"]}
+    return response_final
 
